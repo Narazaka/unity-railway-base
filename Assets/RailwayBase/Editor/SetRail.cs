b0f04d7765dba875ab06363040669055f2125c2e
Narazaka <info@narazaka.net>
2018-09-22 12:43:40 +0900

    ex

---
+++ Assets/RailwayBase/Editor/SetRail.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using UnityEditor;
+
+public static class SetRail {
+
+    static Route Route = new Route(new List<RouteSegment> {
+        new StraightRouteSegment(800),
+        new CurveRouteSegment(30, 400),
+        new StraightRouteSegment(100, 20),
+        new CurveRouteSegment(30, 400),
+        new StraightRouteSegment(100, 20),
+        new CurveRouteSegment(30, 400),
+        new StraightRouteSegment(100, 20),
+        new StraightRouteSegment(100, 20),
+        new StraightRouteSegment(10000, 2),
+    });
+
+    [MenuItem("RailRoad/Make")]
+    static void Set() {
+        var railObjects = Route.ToSubSegments().ToRailObjects();
+    }
+}
